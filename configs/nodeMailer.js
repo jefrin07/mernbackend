0270500173ac45cdca8826fadb5656a6433058a7
jefrin07 <jefrin.ferolous08@gmail.com>
2025-08-29 14:30:13 +0530

    email confirmation test2

--- configs/nodeMailer.js
+++ configs/nodeMailer.js
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com", // or your SMTP host
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
