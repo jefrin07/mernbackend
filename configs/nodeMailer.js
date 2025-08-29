49a81014216e43672661ad18b63712a772746515
jefrin07 <jefrin.ferolous08@gmail.com>
2025-08-29 15:14:43 +0530

    email confirmation test4

--- configs/nodeMailer.js
+++ configs/nodeMailer.js
@@ -1,9 +1,9 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // or your SMTP host
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
